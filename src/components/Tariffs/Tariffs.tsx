f1b88fd84229925627b9d5aaaa10891742289871
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2024-03-21 17:11:50 +0500

    fix: исправлены ошибки сборки

--- src/components/Tariffs/Tariffs.tsx
+++ src/components/Tariffs/Tariffs.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './styles.module.css';
 import { Tariff } from '../../models/models';
-import { CARDTEXT, MOBILE_CARDTEXT } from '../../utils/constans';
 
 interface TariffProps {
   id: number;
